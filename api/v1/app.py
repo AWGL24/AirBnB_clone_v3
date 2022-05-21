4e58919089721847894bc258783c78d74610fbcd
afiorg9000 <3533@holbertonschool.com>
2022-05-21 17:47:49 +0000

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,17 +6,17 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
-host = getenv('HBNB_API_HOST', '0.0.0.0')
-port = getenv('HBNB_API_PORT', 5000)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def tear(exception):
     """ tear method """
-    storage.close()
+    models.storage.close()
 
 
 if __name__ == "__main__":
     setup_global_errors()
-    app.run(host=host, port=port)
+    host = getenv("HBNB_API_HOST") or '0.0.0.0'
+    port = getenv("HBNB_API_PORT") or '5000'
+    app.run(host=host, port=port, threaded=True)

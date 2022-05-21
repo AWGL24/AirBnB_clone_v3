f08e80ebfc701d10c578fbdf8b8a1a15f558f680
afiorg9000 <3533@holbertonschool.com>
2022-05-21 18:27:49 +0000

    Status: ok

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,11 +12,10 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def tear(exception):
     """ tear method """
-    models.storage.close()
+    storage.close()
 
 
 if __name__ == "__main__":
-    setup_global_errors()
     host = getenv("HBNB_API_HOST") or '0.0.0.0'
     port = getenv("HBNB_API_PORT") or '5000'
     app.run(host=host, port=port, threaded=True)

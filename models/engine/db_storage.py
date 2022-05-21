bdedc13afee7558bc14b9a045275488582061bb6
Drew-24 <andrewwil1@hotmail.com>
2022-05-21 17:06:53 -0400

    Fixed importantion and methods in file storage and db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,27 +71,21 @@ class DBStorage:
         Session = scoped_session(sess_factory)
         self.__session = Session
 
-    def close(self):
-        """call remove() method on the private session attribute"""
-        self.__session.remove()
-
     def get(self, cls, id):
         """ Returns the object based on the class and id"""
-        if cls not in classes.values():
-            return None
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
-            if (value.id == id):
-                return value
+        obj = self.all(cls)
+        for key in obj.values():
+            if (key.id == id):
+                return key
         return None
 
     def count(self, cls=None):
         """ Returns the number of objects in storage matching the given class.
         If no class is passed, returns the count of all objects in storage. """
-        if cls is None:
-            return len(models.storage.all())
-        elif cls not in classes.values():
-            return 0
-        else:
-            all_cls = models.storage.all(cls)
-            return len(all_cls.values())
+        if cls:
+            return len(self.all(cls))
+        return len(self.all())
+
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.remove()

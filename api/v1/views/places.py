6305c22e125b500da80c55cfbe075a3b31d88f19
Drew-24 <andrewwil1@hotmail.com>
2022-05-24 10:23:30 -0400

    Fixed code

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""new view for place objects that handles RESTFul API actions"""
+"""
+Module handels new view for place objects that handles RESTFul API actions
+"""
 
-from tkinter import Place
 from flask import jsonify, abort, request
 from models import storage
 from models.city import City
 from models.state import State
+from models.place import Place
 from models.amenity import Amenity
 from api.v1.views import app_views, index
 
